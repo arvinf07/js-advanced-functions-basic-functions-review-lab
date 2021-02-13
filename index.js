263733ade5a19f3ba64690df5c2a58557ab01c06
Arvin Fernandez <arvinf07@gmail.com>
2021-02-12 21:34:24 -0500

    commit tree 2d625ca5f02488ec3b0f27761291690bbb4d636d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity="go to the office"){
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(string="*"){
+  return function(thing='special'){
+    return `You are ${string}${thing}${string}!`
+  }
+}
+
+let Calculator = {
+  add: () => 1 + 3 , 
+  subtract: () => 1 - 3,
+  multiply: () => 1 * 3,
+  divide: () => 10/5
+}
+
+function actionApplyer(start, array){
+  
+  if (!array) {
+    return start
+  }else{
+    return array.reduce(function(total, element){ return total = element(total)}, start)
+  }
+
+}
